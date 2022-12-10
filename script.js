e76b31ac01055db83235e27d456a67ba9a9a2bff
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-10 15:26:15 +0530

    Tune can be listened

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-let audio = new Audio();
+let audio = new Audio("tunes/a.wav");
 
 const playTune = (key) => {
-
+    audio.src = `tunes/${key}.wav`;
+    audio.play();
 }
 
 pianoKeys.forEach(key => {
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
+
+
+
+document.addEventListener("keydown", pressedKey);
